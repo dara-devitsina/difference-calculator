691c9007c353fe385fbfa066838488ff49ef6118
dara <dashadevitsina@gmail.com>
2020-07-01 15:59:39 +0300

    write draft for genDiff()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
-const { program } = require('commander');
-program.version('0.0.1');
+import _ from 'lodash';
 
+const genDiff = (file1, file2) => {
+const entries1 = Object.entries(file1);
+const entries2 = Object.entries(file2);
+const entries = (entries1.concat(entries2));
+const result = [];
+
+	for (const [key, value] of entries) {
+		if (key[value] === key[value]) {
+			const str = `${key}: ${value}`;
+			result.push(str);
+		}
+		if (!_.has(file2, key)) {
+			const str = [key, value].join(': ');
+			result.push(`- ${str}`);
+		}
+		if (!_.has(file1, key)) {
+			const str = [key, value].join(': ');
+			result.push(`+ ${str}`);
+		}
+		if (file1[key] !== file2[key]) {
+			const str = `+ ${key}: ${file2[key]}`;
+			result.push(str);
+		}
+	}
+
+// console.log(result.join('\n'));
+console.log(result);
+
+
+};
+
+const json1 = {
+	"host": "hexlet.io",
+	"timeout": 50,
+	"proxy": "123.234.53.22",
+	"follow": false
+};
+
+const json2 = {
+	"timeout": 20,
+	"verbose": true,
+	"host": "hexlet.io"
+};
+
+console.log(genDiff(json1, json2));
+
+// {
+//	host: hexlet.io
+// + timeout: 20
+// - timeout: 50
+// - proxy: 123.234.53.22
+// + verbose: true
+// - follow: false
+// }
