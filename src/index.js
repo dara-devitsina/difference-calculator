8f83f241ab03dd88c9f159c01289a1be577162f4
dara <dashadevitsina@gmail.com>
2020-07-02 08:34:46 +0300

    refactor genDiff()

--- src/index.js
+++ src/index.js
@@ -1,32 +1,34 @@
 import _ from 'lodash';
+import fs from 'fs';
+import path from 'path';
 
 const genDiff = (file1, file2) => {
-const keys1 = Object.keys(file1);
-const keys2 = Object.keys(file2);
-const keys = _.uniq((keys1.concat(keys2)));
+	const keys1 = Object.keys(file1);
+	const keys2 = Object.keys(file2);
+	const keys = _.uniq((keys1.concat(keys2)));
 
-const result = [];
-	for (const key of keys) {
+	const cb = (acc, key) => {
 		const positive = `+ ${key}: ${file2[key]}`;
 		const negative = `- ${key}: ${file1[key]}`;
 		const neutral = `  ${key}: ${file1[key]}`;
 
 		if (_.has(file1, key) && _.has(file2, key)) {
 			if (file1[key] !== file2[key]) {
-				result.push(positive);
-				result.push(negative);
+				return [...acc, positive, negative];
 			} else {
-				result.push(neutral);
+				return [...acc, neutral];
 			}
 		}
-			if (!_.has(file2, key)) {
-				result.push(negative);
-			}
-			if (!_.has(file1, key)) {
-				result.push(positive);
-			}
-	}
-return result.join('\n');
+		if (!_.has(file2, key)) {
+			return [...acc, negative];
+		}
+		if (!_.has(file1, key)) {
+			return [...acc, positive];
+		}
+	};
+	const changes = keys.reduce(cb, []);
+	const result = ['{', ...changes, '}'];
+	return result.join('\n');
 };
 
 const json1 = {
