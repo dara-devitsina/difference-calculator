4f8934e6e21ddd4ea008f9f3a7a32feefeb2360f
dara <dashadevitsina@gmail.com>
2020-07-05 21:51:58 +0300

    refactor genDiff

--- src/index.js
+++ src/index.js
@@ -1,38 +1,87 @@
 import _ from 'lodash';
 import parse from './parsers.js';
 
-const genDiff = (before, after) => {
+ const genDiff = (before, after) => {
   const file1 = parse(before);
   const file2 = parse(after);
   const keys1 = Object.keys(file1);
   const keys2 = Object.keys(file2);
   const keys = _.uniq(keys1.concat(keys2));
 
-  const cb = (acc, key) => {
-		if (_.has(file1, key) && _.has(file2, key)) {
-			if (file1[key] !== file2[key]) {
-				return [...acc, { [`+ ${key}`]: file2[key] }, { [`- ${key}`]: file1[key] }];
-			} else {
-				return [...acc, { [`  ${key}`]: file1[key] }];
-			}
-		}
-		if (!_.has(file2, key)) {
-			return [...acc, { [`- ${key}`]: file1[key] }];
-		}
-		if (!_.has(file1, key)) {
-			return [...acc, { [`+ ${key}`]: file2[key] }];
-    } 
-	};
-  const changes = keys.reduce(cb, []);
-  
-  const normalized = changes.map((item) => {
-    const [key, value] = Object.entries(item).flat();
-    return `${key}: ${value}`;
-  })
+  const changes = keys.reduce((acc, key) => {
+    const char = 'char';
+    if (_.has(file1, key) && _.has(file2, key)) {
+      if (file1[key] !== file2[key]) {
+        return [...acc, { [key]: file2[key], [char]: '+' }, { [key]: file1[key], [char]: '-' }];
+      }
+        return [...acc, { [key]: file1[key], [char]: '  ' }];
+    }
+    if (!_.has(file2, key)) {
+      return [...acc, { [key]: file1[key], [char]: '-' }];
+    }
+    if (!_.has(file1, key)) {
+      return [...acc, { [key]: file2[key], [char]: '+' }];
+    }
+  }, []);
 
-  const result = `{\n${normalized.join('\n')}\n}`;
-  return result;
+  console.log(changes);
+ // const normalized = changes.map((item) => {
+ //   const [key, value] = Object.entries(item).flat();
+ //   return `${key}: ${value}`;
+ // })
 
+ // const result = `{\n${normalized.join('\n')}\n}`;
+ // return result; 
+};
+export default genDiff; 
+
+const before = {
+  "common": {
+    "setting1": "Value 1",
+    "setting2": 200,
+    "setting3": true,
+    "setting6": {
+      "key": "value"
+    }
+  },
+  "group1": {
+    "baz": "bas",
+    "foo": "bar",
+    "nest": {
+      "key": "value"
+    }
+  },
+  "group2": {
+    "abc": 12345
+  }
 };
 
-export default genDiff;
+const after = {
+  "common": {
+    "follow": false,
+    "setting1": "Value 1",
+    "setting3": {
+      "key": "value"
+    },
+    "setting4": "blah blah",
+    "setting5": {
+      "key5": "value5"
+    },
+    "setting6": {
+      "key": "value",
+      "ops": "vops"
+    }
+  },
+
+  "group1": {
+    "foo": "bar",
+    "baz": "bars",
+    "nest": "str"
+  },
+
+  "group3": {
+    "fee": 100500
+  }
+}
+
+//console.log(genDiff(before, after));
