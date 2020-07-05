60d72778ae8ff81d265b29cdb81801fabe9b6ab3
dara <dashadevitsina@gmail.com>
2020-07-05 16:50:39 +0300

    refactor genDiff once again

--- src/index.js
+++ src/index.js
@@ -8,30 +8,31 @@ const genDiff = (before, after) => {
   const keys2 = Object.keys(file2);
   const keys = _.uniq(keys1.concat(keys2));
 
-  const data = keys.map((key) => {
-    if (_.has(file1, key)) {
-      return { [key]: file1[key] };
-    } return { [key]: file2[key] };
-  });
-  console.log(data);
+  const cb = (acc, key) => {
+		if (_.has(file1, key) && _.has(file2, key)) {
+			if (file1[key] !== file2[key]) {
+				return [...acc, { [`+ ${key}`]: file2[key] }, { [`- ${key}`]: file1[key] }];
+			} else {
+				return [...acc, { [`  ${key}`]: file1[key] }];
+			}
+		}
+		if (!_.has(file2, key)) {
+			return [...acc, { [`- ${key}`]: file1[key] }];
+		}
+		if (!_.has(file1, key)) {
+			return [...acc, { [`+ ${key}`]: file2[key] }];
+    } 
+	};
+  const changes = keys.reduce(cb, []);
   
-
-  const cb = (acc, item) => {
+  const normalized = changes.map((item) => {
     const [key, value] = Object.entries(item).flat();
-    if (_.has(file1, key) && _.has(file2, key)) {
-      if (file1[key] !== file2[key]) {
-        return [...acc, `+ ${key}: ${file2[key]}`, `- ${key}: ${file1[key]}`]
-      }
-      return [...acc, `  ${key}: ${value}`];
-    }
-    if (!_.has(file2, key)) {
-      return [...acc, `- ${key}: ${value}`];
-    }
-    return [...acc, `+ ${key}: ${value}`];
-  };
-  const changes = data.reduce(cb, []);
-  const result = `{\n${changes.join('\n')}\n}`;
- // return result;
+    return `${key}: ${value}`;
+  })
+
+  const result = `{\n${normalized.join('\n')}\n}`;
+  return result;
+
 };
 
 export default genDiff;
