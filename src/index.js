596f262bcef0e9f9d3587931589ebb8c8e2ddfe9
dara <dashadevitsina@gmail.com>
2020-07-09 16:00:08 +0300

    adjust spaces

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ const stringify = (item, currentDepth) => {
   if (_.isObject(item)) {
     const space = ' ';
     const [key, value] = Object.entries(item).flat();
-      return `{\n${space.repeat(currentDepth + 4)}${key}: ${value}\n${space.repeat(currentDepth + 2)}}`;
+      return `{\n${space.repeat(currentDepth + 8)}${key}: ${value}\n${space.repeat(currentDepth + 4)}}`;
   }
   return item;
 };
@@ -42,16 +42,16 @@ const stylish = (tree) => {
   const result = node.reduce((acc, obj) => {
     if (obj.status !== 'nested object') {
       if (obj.status === 'added') {
-        return [...acc, `${space.repeat(depth)}+ ${obj.name}: ${stringify(obj.value, depth)}`];
+        return [...acc, `${space.repeat(depth + 2)}+ ${obj.name}: ${stringify(obj.value, depth)}`];
       } if (obj.status === 'deleted') {
-        return [...acc, `${space.repeat(depth)}- ${obj.name}: ${stringify(obj.value, depth)}`];
+        return [...acc, `${space.repeat(depth + 2)}- ${obj.name}: ${stringify(obj.value, depth)}`];
       } if (obj.status === 'modified') {
-        return [...acc, `${space.repeat(depth)}+ ${obj.name}: ${stringify(obj.after, depth)}\n${space.repeat(depth)}- ${obj.name}: ${stringify(obj.before, depth)}`];
+        return [...acc, `${space.repeat(depth + 2)}+ ${obj.name}: ${stringify(obj.after, depth)}\n${space.repeat(depth + 2)}- ${obj.name}: ${stringify(obj.before, depth)}`];
       } if (obj.status === 'unmodified') {
-        return [...acc, `${space.repeat(depth + 2)}${obj.name}: ${stringify(obj.value, depth)}`];
+        return [...acc, `${space.repeat(depth + 4)}${obj.name}: ${stringify(obj.value, depth)}`];
       }
     }
-    return [...acc, `${space.repeat(depth + 2)}${obj.name}: ${iter(obj.children, depth + 2)}`];
+    return [...acc, `${space.repeat(depth + 4)}${obj.name}: ${iter(obj.children, depth + 4)}`];
   }, []);
 
   return `{\n${result.join('\n')}\n${space.repeat(depth)}}`;
