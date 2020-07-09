d351dbe46f659670983258a513bb23a0da370130
dara <dashadevitsina@gmail.com>
2020-07-09 15:37:55 +0300

    try to adjust spaces

--- src/index.js
+++ src/index.js
@@ -27,29 +27,36 @@ const getDiff = (file1, file2) => {
 const stringify = (item) => {
   if (_.isObject(item)) {
     const [key, value] = Object.entries(item).flat();
-    return `{\n${key}: ${value}\n}`;
+      return `{\n${key}: ${value}\n}`;
   }
   return item;
 };
 
 const stylish = (tree) => {
   // console.log(tree);
-  const result = tree.reduce((acc, obj) => {
+  const iter = (node, depth) => {
+    const space = ' ';
+    //console.log(depth);
+
+  const result = node.reduce((acc, obj) => {
     if (obj.status !== 'nested object') {
       if (obj.status === 'added') {
-        return [...acc, `+ ${obj.name}: ${stringify(obj.value)}`];
+        return [...acc, `${space.repeat(depth)}+ ${obj.name}: ${stringify(obj.value)}`];
       } if (obj.status === 'deleted') {
-        return [...acc, `- ${obj.name}: ${stringify(obj.value)}`];
+        return [...acc, `${space.repeat(depth)}- ${obj.name}: ${stringify(obj.value)}`];
       } if (obj.status === 'modified') {
-        return [...acc, `+ ${obj.name}: ${stringify(obj.after)}\n- ${obj.name}: ${stringify(obj.before)}`];
+        return [...acc, `${space.repeat(depth)}+ ${obj.name}: ${stringify(obj.after)}\n${space.repeat(depth)}- ${obj.name}: ${stringify(obj.before)}`];
       } if (obj.status === 'unmodified') {
-        return [...acc, `  ${obj.name}: ${stringify(obj.value)}`];
+        return [...acc, `${space.repeat(depth + 2)}${obj.name}: ${stringify(obj.value)}`];
       }
     }
-    return [...acc, `  ${obj.name}: ${stylish(obj.children)}`];
-  }, [])
+    return [...acc, `${space.repeat(depth + 2)}${obj.name}: ${iter(obj.children, depth + 2)}`];
+  }, []);
 
-  return `{\n${result.join('\n')}\n}`;
+  return `{\n${result.join('\n')}\n${space.repeat(depth)}}`;
+
+}
+return iter(tree, 0)
 };
 
 const genDiff = (path1, path2) => {
