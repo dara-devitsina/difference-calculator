b7f55638562a8c3811b99ad69c2ea91af3e52c6a
dara <dashadevitsina@gmail.com>
2020-07-14 21:40:30 +0300

    fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,36 +6,28 @@ import genDiff from '../src/index.js';
 const getFixturePath = (filename) => path.join(process.cwd(), '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-let resultRecursive;
-let resultPlain;
-let resultJson;
+const resultRecursive = readFile('result-recursive').trimEnd();
+const resultPlain = readFile('result-plain').trimEnd();
+const resultJson = readFile('result-json').trimEnd();
 
-beforeAll(() => {
-  resultRecursive = readFile('result-recursive').trimEnd();
-  resultPlain = readFile('result-plain').trimEnd();
-  resultJson = readFile('result-json').trimEnd();
-});
+const cases = [
+  [getFixturePath('file1.json'), getFixturePath('file2.json'), '', resultRecursive],
+  [getFixturePath('file1.yml'), getFixturePath('file2.yml'), '', resultRecursive],
+  [getFixturePath('file1.ini'), getFixturePath('file2.ini'), '', resultRecursive],
+  [getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain', resultPlain],
+  [getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'plain', resultPlain],
+  [getFixturePath('file1.ini'), getFixturePath('file2.ini'), 'plain', resultPlain],
+  [getFixturePath('file1.json'), getFixturePath('file2.json'), 'json', resultJson],
+  [getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'json', resultJson],
+  [getFixturePath('file1.ini'), getFixturePath('file2.ini'), 'json', resultJson],
+];
 
-test('compare 2 json files', () => {
-  const before = getFixturePath('file1.json');
-  const after = getFixturePath('file2.json');
-  expect(genDiff(before, after)).toEqual(resultRecursive);
-  expect(genDiff(before, after, 'plain')).toEqual(resultPlain);
-  expect(genDiff(before, after, 'json')).toEqual(resultJson);
-});
-
-test('compare 2 yaml files', () => {
-  const before = getFixturePath('file1.yml');
-  const after = getFixturePath('file2.yml');
-  expect(genDiff(before, after)).toEqual(resultRecursive);
-  expect(genDiff(before, after, 'plain')).toEqual(resultPlain);
-  expect(genDiff(before, after, 'json')).toEqual(resultJson);
-});
-
-test('compare 2 ini files', () => {
-  const before = getFixturePath('file1.ini');
-  const after = getFixturePath('file2.ini');
-  expect(genDiff(before, after)).toEqual(resultRecursive);
-  expect(genDiff(before, after, 'plain')).toEqual(resultPlain);
-  expect(genDiff(before, after, 'json')).toEqual(resultJson);
+describe('test', () => {
+  test.each(cases)(
+    'given %p and %p as arguments with format %p, returns %p',
+    (firstArg, secondArg, thirdArg, expected) => {
+      const result = genDiff(firstArg, secondArg, thirdArg);
+      expect(result).toEqual(expected);
+    },
+  );
 });
