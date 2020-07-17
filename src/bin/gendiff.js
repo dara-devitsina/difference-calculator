a2ffaaa7ef66f514ba5b78346460c34465960016
dara <dashadevitsina@gmail.com>
2020-07-17 09:21:09 +0300

    reconfig package

---
+++ src/bin/gendiff.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+import pkg from 'commander';
+
+import genDiff from '../index.js';
+
+const { program } = pkg;
+
+program
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [stylish, plain, json]', 'output format', 'stylish')
+  .action((filepath1, filepath2) => {
+    console.log(`\n${genDiff(filepath1, filepath2, program.format)}\n`);
+  })
+  .parse(process.argv);
