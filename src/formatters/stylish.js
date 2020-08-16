6863c0350649c8548016e65154c2e4d6e27876a6
dara <dashadevitsina@gmail.com>
2020-08-16 07:24:11 +0300

    refactor stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -13,10 +13,13 @@ const closeSymbol = '}';
 const addPrefix = (symbol, indent, prefix = ' ') => `${indent}${prefix} ${symbol}`;
 
 const stringify = (node, space, depth = 0) => {
+ // console.log(depth);
   if (!_.isObject(node)) {
     return node;
   }
   const entries = Object.entries(node);
+ // console.log(entries);
+
   return [
     openSymbol,
     ...(entries.flatMap(([key, value]) => `${addPrefix(key, space.repeat(depth + 1))}: ${stringify(value, space, depth + 1)}`)),
@@ -27,33 +30,36 @@ const stringify = (node, space, depth = 0) => {
 const stylish = (diffTree) => {
   const iter = (tree, depth) => {
     const indent = indentSymbol.repeat(depth);
-    const objectIndent = indentSymbol.repeat(depth + keyOffset);
-    // console.log(depth + keyOffset);
 
     const result = tree.flatMap((item) => {
+     // console.log(depth);
+
       switch (item.type) {
         case 'added':
           // формируем строку вида `{отступ}{префикс} {имя}: {значение}`
-          return `${addPrefix(item.name, indent, prefixes.added)}: ${stringify(item.value, objectIndent)}`;
+          return `${addPrefix(item.name, indent, prefixes.added)}: ${stringify(item.value, indentSymbol, depth)}`;
         case 'deleted':
-          return `${addPrefix(item.name, indent, prefixes.deleted)}: ${stringify(item.value, objectIndent)}`;
+          return `${addPrefix(item.name, indent, prefixes.deleted)}: ${stringify(item.value, indentSymbol, depth)}`;
         case 'modified':
-          return `${addPrefix(item.name, indent, prefixes.deleted)}: ${stringify(item.value1, objectIndent)}\n${addPrefix(item.name, indent, prefixes.added)}: ${stringify(item.value2, objectIndent)}`;
+          return [
+            `${addPrefix(item.name, indent, prefixes.deleted)}: ${stringify(item.value1, indentSymbol, depth)}`,
+            `${addPrefix(item.name, indent, prefixes.added)}: ${stringify(item.value2, indentSymbol, depth)}`,
+          ];
         case 'unmodified':
-          return `${addPrefix(item.name, indent, prefixes.unmodified)}: ${stringify(item.value, objectIndent)}`;
+          return `${addPrefix(item.name, indent, prefixes.unmodified)}: ${stringify(item.value, indentSymbol, depth)}`;
         case 'nested':
           return [
             `${addPrefix(item.name, indent, prefixes.unmodified)}: ${openSymbol}`,
-            ...(iter(item.children, depth + keyOffset)),
+            ...(iter(item.children, depth + 2)),
             `${addPrefix(closeSymbol, indent)}`,
-          ].join('\n');
+          ];
         default:
           throw new Error(`Unknown type: '${item.type}'!`);
       }
     });
     return result;
   };
-  return `{\n${iter(diffTree, 1).join('\n')}\n}`;
+  return `${openSymbol}\n${iter(diffTree, 1).join('\n')}\n${closeSymbol}`;
 };
 
 export default stylish;
