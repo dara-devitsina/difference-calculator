1c3fd6516b0e0bbded860478ec7f6f26543e87eb
dara <dashadevitsina@gmail.com>
2020-08-19 10:53:09 +0300

    fix tests

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import parse from './src/parsers.js';
 import format from './src/formatters/index.js';
 import buildDiff from './src/diffBuilder.js';
 
-const genDiff = (filePath1, filePath2, outputFormat) => {
+const genDiff = (filePath1, filePath2, outputFormat = 'stylish') => {
   const file1 = fs.readFileSync(filePath1, 'utf8');
   const file2 = fs.readFileSync(filePath2, 'utf8');
   const inputFormat1 = path.extname(filePath1).slice(1);
